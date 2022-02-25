f363f81c7d25db69c650a3c2fe69ea7aa90ab7a2
ProPranu6 <74192300+ProPranu6@users.noreply.github.com>
2022-02-25 15:03:15 +0530

    Add files via upload

---
+++ elite_ai_ml_bckend/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+print("Package Dependencies That Can Cause Error")
+print(" 'requests', 'bs4', 'html5lib', 'tensorflow==2.0', 'tensorflow_hub', 'bert-for-tf2', 'sentencepiece', \n'sentence-transformers', 'transformers', 'sumy'\n")
+from .elite_ai_web_scraping import *
+from .elite_ai_content_summarisation import *
+
+
+
+
+# In[ ]:
+
+
+
+
